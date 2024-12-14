5fcfffc2d6a7ff9517e39f89a49b8df66d4a7105
Wooseong Lee <123723192+ewoooo@users.noreply.github.com>
2024-12-14 17:38:51 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,34 @@
 let modules = [];
 
 function setup() {
-  createCanvas(1280, 720);
-  background(0);
+	createCanvas(1280, 720);
+	background(0);
 
-  textStyle(BOLD);
-  textSize(400);
-  textAlign(CENTER, CENTER);
-  fill(255);
-  text("Hello!", width / 2, height / 2 + 50);
+	textStyle(BOLD);
+	textSize(400);
+	textAlign(CENTER, CENTER);
+	fill(255);
+	text("Hello", width / 2, height / 2 + 50);
 
-  let gap = 10;
+	let gap = 10;
 
-  for (let y = 0; y < height; y += gap) {
-    for (let x = 0; x < width; x += gap) {
-      let c = get(x, y);
-      let b = brightness(c);
+	for (let y = 0; y < height; y += gap) {
+		for (let x = 0; x < width; x += gap) {
+			let c = get(x, y);
+			let b = brightness(c);
 
-      if (b > 50) {
-        modules.push(new Module(new createVector(x, y)))
-      }
-    }
-
-  }
-      console.log(modules)
+			if (b > 50) {
+				modules.push(new Module(new createVector(x, y)));
+			}
+		}
+	}
 }
 
 function draw() {
-  background(240);
-  
-  for (let module of modules) {
-    module.update();
-    module.display();
-  }
+	background(240);
+
+	for (let module of modules) {
+		module.update();
+		module.display();
+	}
 }
